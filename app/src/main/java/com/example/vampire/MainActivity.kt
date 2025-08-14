522d7459086438e6b63d40a039212b01d8d22f1f
Emma <emlcliffe@gmail.com>
2025-08-14 14:27:51 +0100

    Bottom Navigation Menu added

--- app/src/main/java/com/example/vampire/MainActivity.kt
+++ app/src/main/java/com/example/vampire/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    //variable declarations
+    private lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -14,7 +20,32 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+            insets }
+
+        //automatically loads in the addFragment
+        loadFragment(AddFragment())
+
+        //handles switching between Fragments
+        bottomNav = findViewById(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.add -> {
+                    loadFragment(AddFragment())
+                    true }
+                R.id.summary -> {
+                    loadFragment(SummaryFragment())
+                    true }
+                R.id.settings -> {
+                    loadFragment(SettingFragment())
+                    true }
+                else -> false } }
+
+
     }
+
+    //this function loads the Fragment given as its parameter onto the screen
+    private fun loadFragment(fragment : Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit() }
 }
