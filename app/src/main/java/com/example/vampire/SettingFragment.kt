9ca8dbdb7debc8bfb90ad1c714dd07b34483430b
Emma <emlcliffe@gmail.com>
2025-08-19 17:29:54 +0100

    large GUI QOL changes

--- app/src/main/java/com/example/vampire/SettingFragment.kt
+++ app/src/main/java/com/example/vampire/SettingFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -23,7 +25,18 @@ class SettingFragment : Fragment() {
         val db = DatabaseProvider.getDatabase(requireContext())
         val buyDao = db.buyDao()
         val deleteButton = view.findViewById<Button>(R.id.deleteButton)
+        val categorySpinner2 = view.findViewById<Spinner>(R.id.categorySpinner2)
 
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(requireContext(), R.array.category_array, R.layout.spinner_item)
+            .also { adapter ->
+                // Specify the layout to use when the list of choices appears.
+                adapter.setDropDownViewResource(R.layout.spinner_item)
+                // Apply the adapter to the spinner.
+                categorySpinner2.adapter = adapter }
+
+
+        //delete button listener
         deleteButton.setOnClickListener {
             lifecycleScope.launch(Dispatchers.IO) {
                 buyDao.clearTable() } }
